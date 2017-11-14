e4b633acc893e14f74c8691a18238cd0e90ab70b
Whitney Norrad <wnorrad@gmail.com>
2017-11-14 00:14:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 const driver = 'bob';
 
-function calledWithExactly(string); {
-  if st
-}
-
-
-it('invokes console.log() with the string "this code was called"', function () {
-      expect(spy.calledWithExactly('this code was called')).to.be.true;
-    });
-
-    it('invokes console.log() with the `driver` variable', function () {
-      expect(spy.calledWithExactly('bob')).to.be.true;
-    });
-
-    it('invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument', function () {
-      expect(spy.calledWithExactly('bob', 'is the driver variable name')).to.be.true;
-    });
-
+console.log('this code was called')
 
